9e549cbb8bc4e26d2697ba5b6321317cfe3ad3f7
Sayed <sayedabdo4900@gmail.com>
2023-08-02 15:11:00 +0300

    finish part 1 frontend

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import Menu from '../components/Menu';
+import Edit from '../imges/edit.png'
+import Delete from '../imges/delete.png'
+import { Link } from 'react-router-dom'
 
 const SinglePost = () => {
   return (
     <div className='single'>
-      <div className="content"></div>
-      <div className="menu"></div>
+      <div className="content">
+        <img src='https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'/>
+      
+        <div className="user">
+          <img src='https://images.pexels.com/photos/17356078/pexels-photo-17356078/free-photo-of-profile-studio-shoot-of-a-man-posing-against-orange-brown-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
+          <div className="info">
+            <span>Sayed</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={'/write?edit=2'}>
+              <img src={Edit}/>
+            </Link>
+            <img src={Delete}/>
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus obcaecati deserunt beatae quas ut doloremque ex quo qui corrupti pariatur, quos perspiciatis ipsa in dolore earum quisquam saepe, hic blanditiis!Lorem
+        </p>
+        <br/>
+        <p>
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi fugiat vero suscipit, officiis, molestiae reiciendis maiores aperiam veniam asperiores eos, eligendi voluptate totam voluptatem. Ad accusamus quis architecto voluptas omnis?
+        </p>
+        <br/>  
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nam quasi rem reprehenderit? Eveniet dolorum expedita libero? Quia consectetur quae voluptas quo repellendus et optio atque laborum! Quas, aut vero.  
+          <br/>
+        </p>
+      </div>
+      <Menu/>
     </div>
   )
 }
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
 
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 const Write = () => {
+  const [value, setValue] = useState('');
+
+  
   return (
-    <div>Write</div>
+    <div className='write-post'>
+      <div className="content">
+        <input type='text' placeholder='Title'/>
+        <div className="editor-container">
+          <ReactQuill  className='editor' theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+
+        <div className='item'>
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input  type="file" name='' id="file" style={{display:"none"}} />
+          <label htmlFor="file" className='file'> Upload image</label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className='item'>
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art" />
+            <label htmlFor="art" >Art</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" value="science" id="science" />
+            <label htmlFor="science">Science</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="technology" id="technology" />
+            <label htmlFor="technology">Technology</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="desing" id="desing" />
+            <label htmlFor="desing">Desing</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" value="food" id="food" />
+            <label htmlFor="food">Food</label>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
